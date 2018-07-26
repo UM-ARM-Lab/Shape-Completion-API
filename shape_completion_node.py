97593b11e800bb9e7fd90689d710632db268f54e
Andrew Price <arprice@gatech.edu>
2018-07-26 14:32:38 -0400

    Added ROS service interface to shape completion node.

--- shape_completion_node.py
+++ shape_completion_node.py
@@ -2,6 +2,7 @@
 
 from shape_complete import Shape_complete
 from std_msgs.msg import ByteMultiArray, MultiArrayDimension
+from mps_voxels.srv import CompleteShape, CompleteShapeRequest, CompleteShapeResponse
 from rospy.numpy_msg import numpy_msg
 
 import rospy
@@ -11,17 +12,35 @@ import numpy as np
 DIM = 64
 
 
+def service_callback(req, args):
+    sc = args
+
+    arr = np.reshape(req.observation.data, tuple(d.size for d in req.observation.layout.dim))
+
+    occ = arr > 0
+    non = arr < 0
+
+    out = sc.complete(occ=occ, non=non, verbose=False)
+
+    resp = CompleteShapeResponse()
+    resp.hypothesis.data = out.flatten().tolist()
+    resp.hypothesis.layout.dim.append(MultiArrayDimension(label='x', size=DIM, stride=DIM*DIM*DIM))
+    resp.hypothesis.layout.dim.append(MultiArrayDimension(label='y', size=DIM, stride=DIM*DIM))
+    resp.hypothesis.layout.dim.append(MultiArrayDimension(label='z', size=DIM, stride=DIM))
+
+    return resp
+
+
 def callback(msg, args):
     sc = args[0]
     pub = args[1]
 
     arr = np.reshape(msg.data, tuple(d.size for d in msg.layout.dim))
-    print rospy.get_name(), "I heard %s"%str(arr)
 
     occ = arr > 0
     non = arr < 0
 
-    out = sc.complete(occ=occ,non=non,verbose=False)
+    out = sc.complete(occ=occ, non=non, verbose=False)
 
     out_msg = ByteMultiArray()
     out_msg.data = out.flatten().tolist()
@@ -35,6 +54,8 @@ def listener():
     rospy.init_node('shape_completer')
 
     sc = Shape_complete(verbose=True)
+
+    server = rospy.Service('complete_shape', CompleteShape, lambda msg: service_callback(msg, sc))
 
     pub = rospy.Publisher('local_occupancy_predicted', numpy_msg(ByteMultiArray), queue_size=10)
     rospy.Subscriber("local_occupancy", numpy_msg(ByteMultiArray), callback, (sc, pub))
